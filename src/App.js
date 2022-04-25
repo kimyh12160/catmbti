1fec747efc975ed703f5b50565068274f469faf1
kimyh12160 <drasis@nate.com>
2022-04-25 21:11:11 +0900

    add route, Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Question from './pages/Question';
+import Result from './pages/Result';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      Cat MBTI
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/question' element={<Question />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </>
   );
 }
 
